566d2fc975785386f136d60aa58683f005fe1e44
acdlc4 <adriandelacruz@computer-one.attlocal.net>
2024-07-04 19:46:20 -0500

    completed jupyter nb for precip & station analysis

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Import the dependencies.
+from flask import Flask, jsonify
 
 
 
@@ -21,10 +22,70 @@
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+# Define what to do when a user hits the index route = "/"
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my 'Home' page!"
+
+
+# Define what to do when a user hits the index route = "/api/v1.0/precipitation"
+@app.route("/api/v1.0/precipitation")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+# Define what to do when a user hits the index route = "/api/v1.0/stations"
+@app.route("/api/v1.0/stations")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+# Define what to do when a user hits the index route = "/api/v1.0/tobs"
+@app.route("/api/v1.0/tobs")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+# Define what to do when a user hits the index route = "/api/v1.0/<start>"
+@app.route("/api/v1.0/<start>")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+# Define what to do when a user hits the index route = "/api/v1.0/<start>/<end>"
+@app.route("/api/v1.0/<start>/<end>")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+# # 1. import Flask
+# from flask import Flask
+
+# # 2. Create an app, being sure to pass __name__
+# app = Flask(__name__)
+
+
+# # 3. Define what to do when a user hits the index route
+# @app.route("/")
+# def home():
+#     print("Server received request for 'Home' page...")
+#     return "Welcome to my 'Home' page!"
+
+
+# # 4. Define what to do when a user hits the /about route
+# @app.route("/about")
+# def about():
+#     print("Server received request for 'About' page...")
+#     return "Welcome to my 'About' page!"
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
